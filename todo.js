b6ebfff38904d3e2c87be4827a2f4677f2bd6400
kowais915 <kowais915@gmail.com>
2021-09-04 02:53:19 +0500

    fixed the css"

--- todo.js
+++ todo.js
@@ -1,5 +1,14 @@
-const but = document.querySelector('button');
+const but1 = document.querySelector('.button1');
 const bod = document.querySelector('body');
-but.addEventListener('click', ()=>{
-bod.classList.add('green');
+const but2 = document.querySelector('.button2');
+
+but1.addEventListener('click', ()=>{
+bod.setAttribute('style', 'background-color: green;')
+
 })
+
+but2.addEventListener('click', ()=>{
+    bod.setAttribute('style', 'background-color: pink;')
+    
+    })
+
