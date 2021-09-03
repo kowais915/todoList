e67af63933523ef97fdf245d0c8b0f3e6773d705
kowais915 <kowais915@gmail.com>
2021-09-04 03:07:09 +0500

    clicking on item now delets it

--- todo.js
+++ todo.js
@@ -1,6 +1,8 @@
 const but1 = document.querySelector('.button1');
 const bod = document.querySelector('body');
 const but2 = document.querySelector('.button2');
+const para = document.querySelectorAll('p');
+
 
 but1.addEventListener('click', ()=>{
 bod.setAttribute('style', 'background-color: green;')
@@ -12,3 +14,9 @@ but2.addEventListener('click', ()=>{
     
     })
 
+    
+para.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+       item.innerHTML = '';
+    })
+})
